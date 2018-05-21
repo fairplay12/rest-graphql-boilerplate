733c25bbea4ecbf31c6e490e1f76b0fb0c97be3b
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2018-05-21 11:32:31 +0300

    Add employee and employer models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# user-model superstructures
+class Employee(models.Model):
+    user = models.OneToOneField(User, related_name='employee')
+    experience = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+
+
+class Employer(models.Model):
+    user = models.OneToOneField(User, related_name='employer')
+    is_super_employer = models.BooleanField(default=False)
+
+    def __str(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
