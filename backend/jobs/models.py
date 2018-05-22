e39a52600828954aea35f34309a5a2cd610e5592
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2018-05-22 19:56:17 +0300

    Create Business model

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Company(models.Model):
     name = models.CharField(max_length=255)
     owner = models.ForeignKey('accounts.Employer', related_name='companies')
+    business = models.ForeignKey(
+        Business,
+        related_name='companies',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
